85966868dbade33c5e73645daad7922d12ef7138
Gebin George <gebin.official@gmail.com>
2025-09-24 17:33:23 +0000

    no need logo in the nav bar

--- src/components/app/landing-header.tsx
+++ src/components/app/landing-header.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link';
-import { Logo } from './logo';
 import { Button } from '../ui/button';
 
 export default function LandingHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-14 max-w-screen-2xl items-center justify-between px-4">
-        <Logo />
+      <div className="container mx-auto flex h-14 max-w-screen-2xl items-center justify-end px-4">
         <Button asChild>
           <Link href="/login">Get Started</Link>
         </Button>
